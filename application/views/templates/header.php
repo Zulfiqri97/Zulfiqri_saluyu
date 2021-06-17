af7b7120b83aea7de614f3a0b97ef58ff1d1ad97
Zulfiqri97 <66582528+Zulfiqri97@users.noreply.github.com>
2021-06-17 07:11:47 +0700

    q

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -181,7 +181,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="../../index3.html" class="brand-link">
-      <img src="<?= base_url('assets/images/saluyu.png'); ?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="<?= base_url('assets/images/saluyu1.png'); ?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Cv.Saluyu - <?= $this->session->userdata('user_type'); ?></span>
     </a>
 
