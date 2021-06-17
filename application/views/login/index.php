3401bdeca2756e4abcde4a1224c0e356f0f1375f
Zulfiqri97 <66582528+Zulfiqri97@users.noreply.github.com>
2021-06-17 07:42:50 +0700

    Update index.php

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -35,8 +35,7 @@
   <div class="card">
     <div class="card-body login-card-body">
       <div class="login-logo">
-      <b style="font-family: monospace;">Cv.Saluyu</b>
-        <!-- <strong style="font-family: monospace;">Cv.Saluyu</strong> -->
+        <strong>Cv.Saluyu</strong>
       </div>
       <!-- /.login-logo -->
       <?php if($message != null) { echo "<p class='text text-danger text-center'>".$message."</p>"; } ?>
