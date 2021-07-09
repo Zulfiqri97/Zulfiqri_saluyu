17a510f74768d94df591c8115265079d726b04cb
Zulfiqri97 <66582528+Zulfiqri97@users.noreply.github.com>
2021-07-09 20:34:58 +0700

    mengubah tampilan login

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -60,7 +60,7 @@
         <div class="row">
           <!-- /.col -->
           <div class="col-12">
-            <button type="submit" class="btn btn-primary btn-block">tes</button>
+            <button type="submit" class="btn btn-primary btn-block">testing</button>
           </div>
           <!-- /.col -->
         </div>
